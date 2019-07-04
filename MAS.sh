b6d7b9ba4162d3f80b6aedc51d8277c42299beba
Monster8080 <43908973+Monster778@users.noreply.github.com>
2019-07-04 18:19:32 +0300

    Add files via upload

---
+++ MAS.sh
@@ -0,0 +1,11 @@
+chmod +x *
+echo Thanks For Use This script 
+echo This script Made in Syria
+echo by Black Monster
+echo put Your Files ex:(.html,.php.....)
+pkg update
+pkg upgrade
+pkg install php
+pkg install python python2  
+mkdir /sdcard/php/
+php -S localhost:8080 -t /sdcard/php
